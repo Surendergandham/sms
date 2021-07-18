0ec456503851caa2d7342e837fe0068688b6739d
Surendergandham <surendergandhamm@gmail.com>
2021-07-18 11:42:19 +0530

    added colon

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print("Welcome to student Management System")
 print(" 1.Student Services \n 2.Teacher Service")
-Userchoice=int(input("Enter the Appropriate Choice "))
+Userchoice=int(input("Enter the Appropriate Choice : "))
 if Userchoice==1:
     print(" 1. Attendance \n 2. Minor Marks \n 3. Hostel Services")
-    innerchoice=int(input("enter the choice in subcategory "))
+    innerchoice=int(input("enter the choice in subcategory : "))
     if innerchoice==1:
         print("Your attendance details are :")
     if innerchoice==2:
